4ed82cb8b69316efd4cd315fec27731b5a82c330
Nick <zzz727272@gmail.com>
2022-04-05 16:10:03 +0300

    first-fix

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ let soundLevel = document.querySelectorAll("audio");
 soundLevel.forEach((sound) => sound.volume = 0.01);
 
 console.log(soundLevel);
+
+$(document).ready(function() {
+    $('.popup-youtube').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        preloader: false,
+
+        fixedContentPos: false
+    });
+});
